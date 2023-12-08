8e9e215e1c0b5d7498ccac44ce206d9dfbc418bf
RaguRam1991 <6262ragu@gmail.com>
2023-12-08 17:02:32 +0530

    hash router

--- src/index.js
+++ src/index.js
@@ -1,22 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
 import { configureStore } from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <Provider store={configureStore({})}>
-      <React.Fragment>
+  <Provider store={configureStore({})}>
+    <React.Fragment>
+      {process.env.NODE_ENV === "production" ? (
+        <HashRouter basename={process.env.PUBLIC_URL}>
+          <App />
+        </HashRouter>
+      ) : (
         <BrowserRouter basename={process.env.PUBLIC_URL}>
           <App />
         </BrowserRouter>
-      </React.Fragment>
-    </Provider>
+      )}
+    </React.Fragment>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
